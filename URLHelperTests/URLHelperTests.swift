c3ab822ea68676ed41d3cb3758f09f4989dea2ed
liu ming <mingliuboy@gmail.com>
2017-09-27 11:34:12 +0800

    add URLHelper project

---
+++ URLHelperTests/URLHelperTests.swift
@@ -0,0 +1,48 @@
+//
+//  URLHelperTests.swift
+//  URLHelperTests
+//
+//  Created by liujianlin on 16/5/20.
+//  Copyright © 2016年 fcgeek. All rights reserved.
+//
+
+import XCTest
+@testable import URLHelper
+
+class URLHelperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    /**
+     test
+     */
+    func testParseUrl() {
+        let urlString = "https://www.google.com.hk/search?q=URLHelper"
+        guard let url = NSURL(string: urlString) else {
+            return
+        }
+        let parameters = URLHelper.parseURLParameters(url)
+        XCTAssertEqual(parameters["q"], "URLHelper", "Parse Url error")
+    }
+    
+}
